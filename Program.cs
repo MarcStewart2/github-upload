bfc34b7d48e27a1397aec491742093bd29e4659f
MarcStewart2 <marc.stewart@perspecta.com>
2020-08-31 15:06:02 -0500

    initial commit - moving project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Auth;
+using Microsoft.Azure.CosmosDB.Table;
+
+
+namespace Generate_SAS_token
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
